a33982d640c07ccefbcd23c07f11db877963c2bb
laryssa-ramos <laryssaramos284@gmail.com>
2022-11-11 22:08:34 -0300

    adicionando tabela

---
+++ hcode/hcode-projeto-usuarios/index.js
@@ -0,0 +1,15 @@
+let userController = new UserController("form-user-create", "table-users");
+
+userController.onSubmit();
+
+
+
+
+
+// document.querySelectorAll("button").forEach(function(){
+
+//         this.addEventListener("click", function(){
+//                 window.alert("CLICOU!!")
+//         })
+
+// })
